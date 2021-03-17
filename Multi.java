55ebc1be061af1b04676b72ee6e5f906142ba39c
DanielPra <59303233+DanielPra@users.noreply.github.com>
2021-03-17 10:41:50 +0200

    Create Multi.java

---
+++ Multi.java
@@ -0,0 +1,21 @@
+public class Multi {
+	public static void main(String[] args){
+		int a = 1;
+		while(a <= 10)
+		{
+			int b = 1;
+			while(b <= 10) {
+				System.out.print(a * b + " ");
+				if(b <= 10) {
+					b++;
+		                }
+				}
+		            if(a<10)
+		            {
+		            System.out.print("\n");
+		            }
+		            a++;
+		        }
+		    }
+		}
+		
